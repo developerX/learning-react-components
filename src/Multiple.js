ff15a129d044a9b47efe9303c72554141d7b92b6
Mansoor Bahramand <mebahramand@gmail.com>
2016-09-21 18:29:28 -0600

    Added multiple example

---
+++ src/Multiple.js
@@ -0,0 +1,20 @@
+export const Photobook = () => {
+  return (
+    <div>
+      <Photo />
+      <PhotoLink />
+    </div>
+  )
+}
+
+export const Photo = () => {
+  return (
+    <div> I am the image </div>
+  )
+}
+
+export const PhotoLink = () => {
+  return (
+    <div> I am the photo link </div>
+  )
+}
